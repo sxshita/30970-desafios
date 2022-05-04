2555f4957183a5f38675f2d002e903df88e08493
unknown <racagnisasha@gmail.com>
2022-05-04 09:43:36 -0300

    desafio2 correction

--- 30970-desafios/Desafio2.js
+++ 30970-desafios/Desafio2.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 
+const fileExist = async (path) => {
+    try {
+        const stats = fs.existsSync(path);
+
+        if (stats == false) {
+            await fs.promises.writeFile(path, "[]");
+        };
+    }
+    catch(err) {
+        console.log(err);
+    };
+};
+
 class Container {
     constructor(name){
-        this.name = name;
-        this.products = [];
+        this.name = `${name}.json`;
+        this.counter = 1;
     };
 
     async save(object){
         try {
-            const id = this.products.length + 1;
+            const productsArray = await this.getAll();
+
+            const id = this.counter;
+            this.counter++;
             object.id = id;
-            this.products.push(object);
-            await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(this.products, null, '\t'));
+            productsArray.push(object);
+
+            await fs.promises.writeFile(this.name, JSON.stringify(productsArray));
             return id;
         } 
         catch {
@@ -26,7 +43,9 @@ class Container {
 
     async getAll(){
         try{
-            let data = await fs.promises.readFile(`${this.name}.json`);
+            await fileExist(this.name);
+
+            let data = await fs.promises.readFile(this.name);
             data = JSON.parse(data);
             return data;
         }
@@ -39,10 +58,10 @@ class Container {
 
     async deleteById(id){
         try {
-            const data = await this.getAll();
-            if(data.length > 0){
-                this.products = data.filter(product => product.id !== id);
-                await fs.promises.writeFile(`${this.name}.json`, JSON.stringify(this.products, null, '\t'));
+            let data = await this.getAll();
+            if(data?.length > 0){
+                data = data.filter(product => product.id !== id);
+                await fs.promises.writeFile(this.name, JSON.stringify(data));
                 return 'Producto eliminado correctamente';
             } else {
                 return 'No hay productos para borrar;'
@@ -56,8 +75,7 @@ class Container {
 
     async deleteAll() {
         try {
-            this.products = [];
-            await fs.promises.unlink(`${this.name}.json`);
+            await fs.promises.unlink(this.name);
             return 'Se borraron todos los productos correctamente.';
         }
         catch(err) {
@@ -68,30 +86,35 @@ class Container {
 
 };
 
-const Mac = {
-    title: 'Macbook Air M1', price: 140000, thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_679546-MLA47180498257_082021-O.webp'
+async function main(){
+    try{
+        const Mac = {
+            title: 'Macbook Air M1', price: 140000, thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_679546-MLA47180498257_082021-O.webp'
+        }
+        
+        const iPhone = {
+            title: 'iPhone 13', price: 200000, thumbnail: 'https://m.media-amazon.com/images/I/61l9ppRIiqL.jpg'
+        }
+        
+        const Apple = new Container('Apple');
+        
+        console.log(await Apple.save(Mac));
+        
+        console.log(await Apple.save(iPhone));
+
+        console.log(await Apple.getById(2));
+
+        console.log(await Apple.getAll());
+
+        console.log(await Apple.deleteById(2));
+
+        console.log(await Apple.deleteAll());
+    }
+    catch(err) {
+        console.log(err)
+    }
 }
 
-const iPhone = {
-    title: 'iPhone 13', price: 200000, thumbnail: 'https://m.media-amazon.com/images/I/61l9ppRIiqL.jpg'
-}
-
-const Apple = new Container('Apple');
-
-//Apple.save(Mac)
-//.then(id => console.log(id)).catch(err => console.log(err));
-
-//Apple.save(iPhone)
-//.then(id => console.log(id)).catch(err => console.log(err));
-
-//Apple.getById(1).then(res => console.log(res));
-
-//Apple.getById(10).then(res => console.log(res));
-
-//Apple.getAll().then(res => console.log(res)).catch(err => console.log(err))
-
-//Apple.deleteById(2).then(res => console.log(res));
-
-//Apple.deleteAll().then(res => console.log(res)).catch(err => console.log(err));
+main();
 
 
